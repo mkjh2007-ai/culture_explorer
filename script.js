75ddfbd732d652ee24b6a2253744e36f8b7942d2
mkjh2007-ai <mkjh2007@gmail.com>
2026-06-20 18:07:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1338,7 +1338,7 @@ function openDetailModal(destId) {
             <p>${dest.monuments}</p>
             <div class="inline-photo-stack">
               <div class="polaroid-frame">
-                <div class="polaroid-img" style="background-image: url('${dest.monumentUrl}'), url('${FALLBACK_IMAGE}'), url('${SVG_FALLBACK}')"></div>
+                <div class="polaroid-img" style="background-image: url('${dest.monumentUrl}')"></div>
                 <div class="polaroid-caption">historical highlight</div>
               </div>
             </div>
